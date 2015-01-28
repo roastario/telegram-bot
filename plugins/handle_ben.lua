8c38956ae9024da49b83a3b755a8f1de1765297f
Stefano Franz <stefano.franz@byhiras.com>
2015-01-28 17:50:24 +0000

    fix call to lower for case were names are missing

--- plugins/handle_ben.lua
+++ plugins/handle_ben.lua
@@ -1,12 +1,3 @@
---
--- Created by IntelliJ IDEA.
--- User: stefanofranz
--- Date: 28/01/15
--- Time: 10:50
--- To change this template use File | Settings | File Templates.
---
-
-
 function appendSender(msg)
     local firstName = string.lower(msg.from.first_name == nil and 'NIL' or msg.from.first_name)
     local lastName = string.lower(msg.from.last_name == nil and 'NIL' or msg.from.last_name)
@@ -25,11 +16,10 @@ function run(msg, matches)
 end
 
 function build_run(http)
-
+    local lo_ht = http
     return function(msg, matches)
-        print ("I WOULDA PROCESSED THE SHIT OUT OF THIS! " .. matches[1])
+        return lo_ht.request("http://localhost:8976/think?text="..matches[1])
     end
-
 end
 
 local http = require("socket.http")
