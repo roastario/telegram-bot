637799a25d7e4191f67b3de189fbe110907989d7
Stefano Franz <stefano.franz@byhiras.com>
2015-01-28 18:00:41 +0000

    disable ben_handler

--- plugins/handle_ben.lua
+++ plugins/handle_ben.lua
@@ -2,7 +2,7 @@ function appendSender(msg)
     local firstName = string.lower(msg.from.first_name == nil and 'NIL' or msg.from.first_name)
     local lastName = string.lower(msg.from.last_name == nil and 'NIL' or msg.from.last_name)
 
-    if (string.find(firstName, "franz") or string.find(lastName, "franz")) then
+    if (string.find(firstName, "davidson") or string.find(lastName, "davidson")) then
         return "!BD " .. msg.text
     else
         return msg.text
@@ -10,7 +10,7 @@ function appendSender(msg)
 end
 
 function run(msg, matches)
-        return http.request("http://localhost:8976/think?text="..matches[1])
+--        return http.request("http://localhost:8976/think?text="..matches[1])
 end
 
 return {
