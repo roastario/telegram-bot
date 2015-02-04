49c476a91058a9819eb40ac03b9a8e940e54c505
Stefano Franz <stefano.franz@byhiras.com>
2015-02-04 18:02:47 +0000

    refactor code to allow commonality between subreddit plugins

--- plugins/bpt.lua
+++ plugins/bpt.lua
@@ -8,5 +8,5 @@
 
 local loading_function = loadfile('plugins/shared_plugin_code/shared_reddit.lua')()
 local loaded_plugin = loading_function('BlackPeopleTwitter', {'^!bpt (.*)', '^!bpt$'})
-return loaded_plugin();
+return loaded_plugin;
 
