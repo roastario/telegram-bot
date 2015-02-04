3d1960ef7ad6ff4f1b43df895240f0d4542b9c82
Stefano Franz <stefano.franz@byhiras.com>
2015-02-04 18:30:09 +0000

    add reaction + nsfw_gifs plugins

--- plugins/bpt.lua
+++ plugins/bpt.lua
@@ -1,4 +1,4 @@
-local loading_function = loadfile('plugins/shared_plugin_code/shared_reddit.lua')()
+local loading_function = loadfile('plugins/shared_plugin_code/shared_reddit.lua', true)()
 local loaded_plugin = loading_function('BlackPeopleTwitter', {'^!bpt (.*)', '^!bpt$'})
 return loaded_plugin;
 
