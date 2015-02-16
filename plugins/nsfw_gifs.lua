604dbbbd6c4482ed3337b1a90c57bf68132a5236
Stefano Franz <stefano.franz@byhiras.com>
2015-02-16 11:12:13 +0000

    attempt to convert png on the fly

--- plugins/nsfw_gifs.lua
+++ plugins/nsfw_gifs.lua
@@ -1,3 +1,3 @@
 local loading_function = loadfile('plugins/shared_plugin_code/shared_reddit.lua')()
-local loaded_plugin = loading_function('nsfw_gif', "^!ng", "^!ng (.*)")
+local loaded_plugin = loading_function('nsfw_gif', "^!ng$", "^!ng (.*)$")
 return loaded_plugin;
